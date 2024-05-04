7272de2af9542ed804eef2f0aa2a1f1e1ad4f6ff
Roni Hossain <roni.cse024@gmail.com>
2024-05-05 02:00:42 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+#from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
+import pickle
+from keras.preprocessing import image
+import numpy as np
+import numpy as np
+import cv2
+import os
+import random
+import sqlite3
+import uuid
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow import keras
+from keras.layers import Dense
+from keras.models import Sequential, load_model
+from PIL import Image
+from tensorflow.keras.models import save_model, load_model
+from keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+@app.route('/predict',methods=['POST'])
+def predictDisease():
+    imagefile= request.files['imagefile']
+    image_path = "./static/images/" + imagefile.filename
+    imagefile.save(image_path)
+    loaded_model = load_model('rice_leaf_model.h5')
+    #new_image_path = '/content/drive/MyDrive/Test_Cat_Dog/cats/cat.4686.jpg'
+    img = image.load_img(image_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    predictions = loaded_model.predict(img_array)
+
+    # Get the class index with the highest probability
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+
+    # Map the class index to class name
+    class_names = ['bacterial_leaf_blight', 'brown_spot', 'healthy', 'leaf_blast', 'leaf_scald', 'narrow_brown_spot']
+    predicted_class_name = class_names[predicted_class_index]
+
+    print("Predicted class:", predicted_class_name)
+
+
+    return render_template('predict.html',image_path=image_path,prediction=predicted_class_name)
+
+@app.route('/analysis')
+def analysis():
+    return render_template('analysis.html')
+@app.route('/charts')
+def charts():
+    return render_template('charts.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
